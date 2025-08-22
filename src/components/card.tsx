a3287e5ae4cd8edfd94051bb83a4742aade6b90b
ShiyaraSanthosh <shiyarasanthosh3@gmail.com>
2025-08-23 03:30:01 +0530

    p

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,6 +2,7 @@ import { getMovie } from "@/api";
 import { useState, useEffect } from "react";
 
 const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+const poster_path = "/x6xiixuQ3FpbEgiu8cr1444H0g7.jpg";
 
 const Card = () => {
   const [moviesList, setMovieList] = useState([]);
@@ -19,13 +20,13 @@ const Card = () => {
   return (
     <div className="bg-white shadow-md w-60 rounded-lg shadow-sm dark:bg-black mt-[8%] ml-[5%]">
       <div className="p-4 space-y-2 rounded-lg shadow-sm dark:bg-black">
-        <ul>
+        <ul className="grid grid-cols-3 gap-6">
           {moviesList.map((movie) => (
             <li key={movie.id} className="mb-6">
               <img
                 src={`${BASE_IMAGE_URL}${movie.poster_path}`} 
                 alt={movie.title}
-                className="w-60 h-80 rounded-lg shadow-md"
+                className="w-60 h-80 rounded-lg shadow-md "
               />
               <h3 className="text-lg font-bold text-gray-800">{movie.title}</h3>
               <p className="text-yellow-600 font-semibold">⭐ {movie.vote_average}</p>
